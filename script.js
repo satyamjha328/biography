e3434b7ed2b3d1e992f404ec19cc77797dd4fc12
Satyam <43123981+satyamjha328@users.noreply.github.com>
2026-02-28 18:58:57 +0530

    Add menu toggle functionality for navigation links

--- script.js
+++ script.js
@@ -59,4 +59,13 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+
+const toggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+if (toggle) {
+    toggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+}
 });
