848b8a901f2fbd74407e683da0c435423dfa76d6
Satyam <43123981+satyamjha328@users.noreply.github.com>
2026-02-28 16:31:50 +0530

    Add scroll event listener to change header style

--- script.js
+++ script.js
@@ -71,3 +71,13 @@ document.addEventListener("mousemove", (e) => {
         glow.style.transform = `translate(${x}px, ${y}px)`;
     }
 });
+
+
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
